0dee91b3b02fd3bd867ec885da90a9aae41ed7c2
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2022-08-24 19:10:48 -0700

    Count given mons as defeated (for type eff display purposes).

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -91,6 +91,7 @@ u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 u
     case MON_GIVEN_TO_PC:
         GetSetPokedexFlag(nationalDexNum, FLAG_SET_SEEN);
         GetSetPokedexFlag(nationalDexNum, FLAG_SET_CAUGHT);
+        GetSetPokedexFlag(nationalDexNum, FLAG_SET_DEFEATED);
         break;
     }
     return sentToPc;
