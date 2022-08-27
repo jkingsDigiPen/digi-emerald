55315323ee5674cb46a59352d67441f19d6e1e54
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2022-08-27 12:19:54 -0700

    Improve petalburg woods grunt AI.

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -148,7 +148,7 @@ const struct Trainer gTrainers[] = {
         .trainerName = _("Grunt"),
         .items = {},
         .doubleBattle = FALSE,
-        .aiFlags = AI_FLAG_CHECK_BAD_MOVE,
+        .aiFlags = AI_FLAG_CHECK_BAD_MOVE | AI_FLAG_TRY_TO_FAINT | AI_FLAG_CHECK_VIABILITY,
         .partySize = ARRAY_COUNT(sParty_GruntPetalburgWoods),
         .party = {.NoItemDefaultMoves = sParty_GruntPetalburgWoods},
     },
