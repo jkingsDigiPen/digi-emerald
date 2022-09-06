8c3e88364e2de08e74ce1eb37e002e7dc63e1812
Jeremy Kings <47997513+jkingsDigiPen@users.noreply.github.com>
2022-09-05 17:35:30 -0700

    Lower puttimon evo level.

--- src/data/pokemon/evolution.h
+++ src/data/pokemon/evolution.h
@@ -245,7 +245,7 @@ const struct Evolution gEvolutionTable[NUM_SPECIES][EVOS_PER_MON] =
     [SPECIES_AGGRON]                = {{EVO_MEGA_EVOLUTION, ITEM_AGGRONITE, SPECIES_AGGRON_MEGA}},
     [SPECIES_LILEEP]                = {{EVO_LEVEL, 40, SPECIES_CRADILY}},
     [SPECIES_ANORITH]               = {{EVO_LEVEL, 40, SPECIES_ARMALDO}},
-    [SPECIES_RALTS]                 = {{EVO_LEVEL, 20, SPECIES_KIRLIA}}, // puttimon
+    [SPECIES_RALTS]                 = {{EVO_LEVEL, 15, SPECIES_KIRLIA}}, // puttimon
     [SPECIES_KIRLIA]                = {{EVO_LEVEL, 30, SPECIES_GARDEVOIR}, // tapirmon
                                        {EVO_ITEM, ITEM_DAWN_STONE, SPECIES_GALLADE}},
     [SPECIES_GARDEVOIR]             = {{EVO_MEGA_EVOLUTION, ITEM_GARDEVOIRITE, SPECIES_GARDEVOIR_MEGA}},
